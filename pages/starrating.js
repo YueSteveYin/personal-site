123995576ead5801c94e330261de412d5a05502b
YueSteveYin <yuy045@ucsd.edu>
2023-12-05 16:10:03 -0800

    in progress

--- pages/starrating.js
+++ pages/starrating.js
@@ -2,29 +2,17 @@ class ratingWidget extends HTMLElement{
     constructor () {
         super();
         const shadow = this.attachShadow({ mode: 'open' });
-        console.log(this.shadowRoot.innerHTML);
 
-        const stars = document.createElement('div');
-        stars.classList.add('stars');
-        stars.setAttribute('data-rating', '0');
-        for (let i = 1; i <= 5; i++) {
-            const star = document.createElement('span');
-            star.classList.add('star');
-            star.setAttribute('data-value', i.toString());
-            star.textContent = '\u2605';
-            stars.appendChild(star);
-        }
+        //create basic element
+        const form = document.createElement('form');
+        // form.setAttribute('action', 'https://httpbin.org/post');
+        // form.setAttribute('method', 'POST');
 
-        let starbutton = this.shadowRoot.querySelectorAll(".star");
-        starbutton.forEach(star => {
-            star.addEventListener('click', () => {
-                const value = star.dataset.value;
-                setRating(value);
-                sendRating(value);
-            });
-        });
+        const title = document.createElement('label');
+        title.setAttribute("id", "rating title")
+        title.setAttribute('for', 'rating');
+        title.textContent = 'How Satisfied are you? ';
         
-
         const ratingInput = document.createElement('input');
         ratingInput.setAttribute('type', 'number');
         ratingInput.setAttribute('id', 'rating');
@@ -34,7 +22,81 @@ class ratingWidget extends HTMLElement{
         ratingInput.setAttribute('value', '0');
         ratingInput.setAttribute('required', '');
 
-        shadow.appendChild(stars);
+        const submit = document.createElement('button');
+        submit.setAttribute('type', 'submit');
+        submit.textContent = 'Submit';
+        submit.style.display = 'none';
+
+        const stars = document.createElement('div');
+        stars.classList.add('stars');
+        stars.setAttribute('data-rating', '0');
+        for (let i = 1; i <= 5; i++) {
+            const star = document.createElement('span');
+            star.setAttribute("class", "star");
+            star.setAttribute('data-value', i.toString());
+            star.textContent = '\u2605';
+            stars.appendChild(star);
+        }
+
+        // Adding functions and css
+
+        const style = document.createElement('style');
+        style.textContent = `
+        .star {
+            font-size: 30px;
+            color: #ccc;
+            transition: color 0.2s;
+            cursor: pointer;
+        }
+        .star:hover{
+            color: gold;
+        }
+        `;
+
+        form.appendChild(title);
+        form.appendChild(stars);
+        form.appendChild(submit);
+
+        shadow.appendChild(form);
+        shadow.appendChild(style);
+
+        let starbutton = this.shadowRoot.querySelectorAll(".star");
+        starbutton.forEach(star => {
+            star.addEventListener('click', () => {
+                const value = star.dataset.value;
+                ratingInput.value = value;
+                for(let i = 0; i<starbutton.length; i++){
+                    if(i<=(value-1)){
+                        starbutton[i].style.color = "gold";
+                    } else{
+                        starbutton[i].style.color = "#ccc";
+                    }
+                }
+                submit.click();
+            });
+        });
+        form.addEventListener('submit', function(event) {
+            event.preventDefault(); // Prevent default form submission behavior
+
+            const formData = new FormData(form);
+            fetch('https://httpbin.org/post', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => {
+                if (response.ok) {
+                console.log('Form submitted successfully');
+                // You can add additional handling here, such as showing a success message
+                } else {
+                console.error('Form submission failed');
+                // Handle error cases if needed
+                }
+            })
+            .catch(error => {3
+                console.error('Error:', error);
+                // Handle error cases if needed
+            });
+        });
     }
 }
 window.customElements.define('rating-widget', ratingWidget);
